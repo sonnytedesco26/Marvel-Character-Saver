93afc28f05cad5220d0168db35f0d98d48cf2881
Sonny Tedesco <sonny.tedesco@darkmatter2bd.com>
2022-06-01 13:51:09 -0400

    wrote up typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,50 +1,39 @@
 const { gql } = require('apollo-server-express');
 //ARRAY inicated with bracket, ! is required
 const typeDefs = gql`
+
   type User {
-    _id: ID
+    userId: ID!
     username: String
     email: String
-    friendCount: Int
-    thoughts: [Thought]
-    friends: [User]
+    characterCount: Int
+    savedCharactera: [Character]
   }
 
-  type Thought {
-    _id: ID
-    thoughtText: String
-    createdAt: String
-    username: String
-    reactionCount: Int
-    reactions: [Reaction]
+  type Character {
+    characterId: ID
+    apiId: String!
   }
 
-  type Reaction {
-    _id: ID
-    reactionBody: String
-    createdAt: String
-    username: String
-  }
+  input SavedCharacters {
+    characterId: ID!
+    userId: ID!
+}
 
   type Auth {
-    token: ID!
-    user: User
+      token: ID!
+      user: User
   }
-
+  
   type Query {
     me: User
-    users: [User]
-    user(username: String!): User
-    thoughts(username: String): [Thought]
-    thought(_id: ID!): Thought
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    addThought(thoughtText: String!): Thought
-    addReaction(thoughtId: ID!, reactionBody: String!): Thought
-    addFriend(friendId: ID!): User
+    saveCharacter(characterData: SavedCharacters): User
+    removeCharacter(characterId: ID!): User
   }
 `;
 
